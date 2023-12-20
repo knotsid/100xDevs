be95cb21fba01cbc3fd2a7df8df3a432eeca6b8d
Not Sid <kumarsid6500@gmail.com>
2023-12-20 23:57:52 +0530

    tested post req

--- week2/Lecture2.2_Codes/express.js
+++ week2/Lecture2.2_Codes/express.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const PORT = 3000;
 const app = express();
+
+app.use(bodyParser.json)
 
 app.get('/', function(req,res){
      res.send("moye moye")
@@ -19,6 +22,30 @@ app.get('/test-html', function(req,res){
 
 app.get('/js', function(req,res){
      res.send("js is fun")
+})
+
+// ########### POST REQUESTS ##################
+
+app.post('/test-header', function(req,res){
+     // adding auth header as 123 in postman
+     console.log(req.headers)
+     res.send({
+          msg: 'testing post req'
+     })
+     //  RESPONSE ON CONSOLE
+     // {
+     //      authorization: '123',
+     //      'content-type': 'application/json',
+     //      'user-agent': 'PostmanRuntime/7.36.0',
+     //      accept: '*/*',
+     //      'postman-token': '4f69b082-4f88-4c3d-9672-94321ba0b8aa',
+     //      host: 'localhost:3000',
+     //      'accept-encoding': 'gzip, deflate, br',
+     //      connection: 'keep-alive',
+     //      'content-length': '36'
+     // }
+     console.log(req.headers["authorization"])    // to get only authorization token
+     console.log(req.body)    // client side input 
 })
 
 app.listen(PORT, function(){
