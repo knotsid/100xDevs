5a71adfa41203c9b794a8a751839453dd53b94fc
Not Sid <kumarsid6500@gmail.com>
2023-12-20 23:33:20 +0530

    tested get request with json and html

--- week2/Lecture2.2_Codes/express.js
+++ week2/Lecture2.2_Codes/express.js
@@ -6,6 +6,17 @@ app.get('/', function(req,res){
      res.send("moye moye")
 })
 
+app.get('/test-json', function(req,res){
+     res.json({
+          name: "notsid",
+          age: 22
+     })
+})
+
+app.get('/test-html', function(req,res){
+     res.send("<h1>JS is fun</h1>")
+})
+
 app.get('/js', function(req,res){
      res.send("js is fun")
 })
